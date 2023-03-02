9ed2c9e057552cfd10d86e1a382ef6aad82530fd
Jessica Godwin <jlg0003@uw.edu>
2023-03-02 08:05:13 -0800

    Thru BetaBin model conjugate and grid approx

---
+++ Figures/MakeFigs.R
@@ -0,0 +1,41 @@
+setwd("~/Dropbox/Workshops/IntroBayesPartII/Figures/")
+set.seed(1234)
+prior_p <- rbeta(1000, 2, 2)
+
+pdf("Gamma22_Prior.pdf", height = 3, width = 3)
+hist(prior_p, main = "", xlab = "p",
+     border = FALSE, col = "navy", freq = FALSE)
+dev.off()
+
+set.seed(202303)
+n <- 100
+age_groups <- c("0-14", "15-64", "65+")
+data <- data.frame(ID = 1:n,
+                   Age = age_groups[sample(1:3, size = 100,
+                                           replace = TRUE,
+                                           prob = c(0.182, 0.65,
+                                                    0.168))])
+post_prob <- rbeta(n = 1000, shape1 = 17, shape2 = 74)
+support_ratio <- (1 - post_prob)/post_prob
+
+
+
+p_grid <- seq(0.001, 0.999, .001)
+prior_eval <- dbeta(p_grid, shape1 = 2, shape2 = 2)
+likelihood_eval <- dbinom(15, size = 87, prob = p_grid)
+marg_calc <- sum(likelihood_eval*prior_eval)
+post_eval <- (1/marg_calc)*likelihood_eval*prior_eval
+
+pdf("Posterior_GridApprox_Mode.pdf", height = 3, width = 3)
+par(mfrow = c(1,1), lend = 1)
+plot(p_grid, post_eval,
+     xlab = "p", ylab = "Density",
+     xlim = c(0.1, 0.3), lwd = 3, type = "l",
+     col = "firebrick")
+abline(h = 0)
+abline(v = p_grid[which.max(post_eval)], lty = 2, lwd = 2)
+legend("topright", bty = "n",
+       lwd = 2, lty = c(1,2), cex = 0.5,
+       col = c("firebrick", "black"),
+       legend = c("Posterior", "Posterior Mode"))
+dev.off()
