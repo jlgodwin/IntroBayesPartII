fd77d3ccdcff5f0cbd708f5795c9d76d6286a3ae
Jessica Godwin <jlg0003@uw.edu>
2023-03-02 09:39:45 -0800

    Finished up and purl()

--- Figures/MakeFigs.R
+++ Figures/MakeFigs.R
@@ -1,4 +1,5 @@
 setwd("~/Dropbox/Workshops/IntroBayesPartII/Figures/")
+library(scales)
 set.seed(1234)
 prior_p <- rbeta(1000, 2, 2)
 
@@ -18,7 +19,7 @@ data <- data.frame(ID = 1:n,
 post_prob <- rbeta(n = 1000, shape1 = 17, shape2 = 74)
 support_ratio <- (1 - post_prob)/post_prob
 
-
+post_pred <- rbinom(1000, size = 87, prob = post_prob)
 
 p_grid <- seq(0.001, 0.999, .001)
 prior_eval <- dbeta(p_grid, shape1 = 2, shape2 = 2)
@@ -38,4 +39,26 @@ legend("topright", bty = "n",
        lwd = 2, lty = c(1,2), cex = 0.5,
        col = c("firebrick", "black"),
        legend = c("Posterior", "Posterior Mode"))
+dev.off()
+.
+
+post_prob_grid <- sample(p_grid, size = 1000, replace = TRUE,
+                         prob = post_eval)
+
+true_post <- dbeta(p_grid, 17, 74)
+pdf("Posterior_Compare.pdf", height = 3, width = 3)
+par(mfrow = c(1,1), lend = 1)
+hist(post_prob, freq = FALSE,
+     main = "", xlab = "p",
+     ylab = "Density",
+     border = FALSE, col = alpha("navy", 0.4))
+hist(post_prob_grid, freq = FALSE,
+     main = "", xlab = "p",
+     ylab = "Density", add = TRUE,
+     border = FALSE, col = alpha("firebrick", 0.4))
+lines(p_grid, true_post, lwd = 2)
+legend("topright", bty = "n",
+       border = FALSE, cex = 0.5,
+       fill = alpha(c("navy", "firebrick"), 0.4),
+       legend = c("Posterior", "Grid Approximation"))
 dev.off()
---
+++ Slides/IntroBayesPartII_20230301.R
@@ -0,0 +1,90 @@
+## ----setup, include=FALSE-------------------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, message = FALSE)
+
+
+## ---- echo = FALSE, results = 'asis'--------------------------------------------------
+library(dplyr)
+library(xtable)
+set.seed(202303)
+n <- 100
+age_groups <- c("0-14", "15-64", "65+")
+data <- data.frame(ID = 1:n,
+                   Age = age_groups[sample(1:3, size = 100,
+                                           replace = TRUE,
+                                           prob = c(0.182, 0.65,
+                                                    0.168))])
+to_print <- data %>% group_by(Age) %>% 
+  summarise(N = n()) %>% 
+  ungroup() %>% 
+  mutate(y = ifelse(Age == "65+", 1,
+                    ifelse(Age == "15-64", 0, NA))) %>% 
+  xtable(., align = "cc|cc", digits = c(0, 0, 0, 0))
+
+print.xtable(to_print, comment = FALSE, include.rownames = FALSE)
+
+
+## ---- echo = TRUE---------------------------------------------------------------------
+qbeta(c(0.025, 0.975), shape1 = 17, shape2 = 74)
+
+
+## ---- echo = TRUE, fig.height = 3, fig.width = 4, fig.align = "center"----------------
+post_prob <- rbeta(n = 1000, shape1 = 17, shape2 = 74)
+hist(post_prob, main = "", xlab = "Proportion Above 15 who are 65+",
+     border = FALSE, col = "navy", freq = FALSE)
+
+
+## ---- echo = TRUE, fig.height = 3, fig.width = 4, fig.align = "center"----------------
+sum(post_prob > 15/87)/length(post_prob)
+sum(post_prob > 0.25)/length(post_prob)
+
+
+## ---- echo = TRUE---------------------------------------------------------------------
+support_ratio <- (1 - post_prob)/post_prob
+c(mean(support_ratio), sd(support_ratio))
+quantile(support_ratio, probs = c(0.025, 0.975))
+
+
+## ---- echo = TRUE---------------------------------------------------------------------
+post_pred <- rbinom(1000, size = 87, prob = post_prob)
+c(mean(post_pred), sd(post_pred))
+quantile(post_pred, probs = c(0.025, 0.975))
+
+
+## ---- echo = TRUE---------------------------------------------------------------------
+p_grid <- seq(0.001, 0.999, .001)
+prior_eval <- dbeta(p_grid, shape1 = 2, shape2 = 2)
+likelihood_eval <- dbinom(15, size = 87, prob = p_grid)
+marg_calc <- sum(likelihood_eval*prior_eval)
+post_eval <- (1/marg_calc)*likelihood_eval*prior_eval
+
+
+## ---- echo = FALSE, fig.height = 3, fig.width = 3, fig.align="center"-----------------
+y_lims <- round(c(min(c(prior_eval/10, likelihood_eval, post_eval)),
+                  max(c(prior_eval/10, likelihood_eval, post_eval))),
+                digits = 3)
+
+par(lend = 1)
+plot(p_grid, prior_eval/10,
+     xlab = "p", ylab = "Density",
+     xlim = c(0,1), ylim = y_lims,
+     lwd = 3, type = "l", col = "grey75")
+lines(p_grid, likelihood_eval,
+      lwd = 3, col = "navy")
+lines(p_grid, post_eval,
+      lwd = 3, col = "firebrick")
+legend("topright", bty = "n", lwd = 3, cex = .5,
+       col = c("grey75", "navy", "firebrick"),
+       legend = c("Prior", "Likelihood", "Posterior"))
+
+
+
+## ---- echo = TRUE---------------------------------------------------------------------
+max_val_idx <- which.max(post_eval)
+p_grid[max_val_idx]
+
+
+## ---- echo = TRUE---------------------------------------------------------------------
+post_samp_grid <- sample(p_grid, size = 1000, replace = TRUE,
+                         prob = post_eval)
+quantile(post_samp_grid, c(0.025, .5, 0.975))
+
