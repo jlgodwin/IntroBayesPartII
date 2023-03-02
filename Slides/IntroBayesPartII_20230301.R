9d87e85d61083605aba3b14db54cd93058cc3ac4
Jessica Godwin <jlg0003@uw.edu>
2023-03-02 10:04:29 -0800

    Update .R

--- Slides/IntroBayesPartII_20230301.R
+++ Slides/IntroBayesPartII_20230301.R
@@ -1,10 +1,11 @@
-## ----setup, include=FALSE-------------------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE, message = FALSE)
+# Setup ####
+rm(list = ls())
 
-
-## ---- echo = FALSE, results = 'asis'--------------------------------------------------
+## Libraries ####
 library(dplyr)
-library(xtable)
+
+# Sample ####
+
 set.seed(202303)
 n <- 100
 age_groups <- c("0-14", "15-64", "65+")
@@ -13,44 +14,34 @@ data <- data.frame(ID = 1:n,
                                            replace = TRUE,
                                            prob = c(0.182, 0.65,
                                                     0.168))])
-to_print <- data %>% group_by(Age) %>% 
-  summarise(N = n()) %>% 
-  ungroup() %>% 
-  mutate(y = ifelse(Age == "65+", 1,
-                    ifelse(Age == "15-64", 0, NA))) %>% 
-  xtable(., align = "cc|cc", digits = c(0, 0, 0, 0))
 
-print.xtable(to_print, comment = FALSE, include.rownames = FALSE)
-
-
-## ---- echo = TRUE---------------------------------------------------------------------
+## Beta posterior quantiles ####
 qbeta(c(0.025, 0.975), shape1 = 17, shape2 = 74)
 
-
-## ---- echo = TRUE, fig.height = 3, fig.width = 4, fig.align = "center"----------------
+## Beta posterior samples ####
 post_prob <- rbeta(n = 1000, shape1 = 17, shape2 = 74)
 hist(post_prob, main = "", xlab = "Proportion Above 15 who are 65+",
      border = FALSE, col = "navy", freq = FALSE)
 
 
-## ---- echo = TRUE, fig.height = 3, fig.width = 4, fig.align = "center"----------------
+## Exceedance probabilities ####
 sum(post_prob > 15/87)/length(post_prob)
 sum(post_prob > 0.25)/length(post_prob)
 
 
-## ---- echo = TRUE---------------------------------------------------------------------
+## Calculting the support ratio ####
 support_ratio <- (1 - post_prob)/post_prob
 c(mean(support_ratio), sd(support_ratio))
 quantile(support_ratio, probs = c(0.025, 0.975))
 
 
-## ---- echo = TRUE---------------------------------------------------------------------
+## Posterior predictive interval ####
 post_pred <- rbinom(1000, size = 87, prob = post_prob)
 c(mean(post_pred), sd(post_pred))
 quantile(post_pred, probs = c(0.025, 0.975))
 
 
-## ---- echo = TRUE---------------------------------------------------------------------
+## Grid approximation ####
 p_grid <- seq(0.001, 0.999, .001)
 prior_eval <- dbeta(p_grid, shape1 = 2, shape2 = 2)
 likelihood_eval <- dbinom(15, size = 87, prob = p_grid)
@@ -58,7 +49,7 @@ marg_calc <- sum(likelihood_eval*prior_eval)
 post_eval <- (1/marg_calc)*likelihood_eval*prior_eval
 
 
-## ---- echo = FALSE, fig.height = 3, fig.width = 3, fig.align="center"-----------------
+## Plot ####
 y_lims <- round(c(min(c(prior_eval/10, likelihood_eval, post_eval)),
                   max(c(prior_eval/10, likelihood_eval, post_eval))),
                 digits = 3)
@@ -78,12 +69,12 @@ legend("topright", bty = "n", lwd = 3, cex = .5,
 
 
 
-## ---- echo = TRUE---------------------------------------------------------------------
+## Posterior mode ####
 max_val_idx <- which.max(post_eval)
 p_grid[max_val_idx]
 
 
-## ---- echo = TRUE---------------------------------------------------------------------
+## Posterior samples ####
 post_samp_grid <- sample(p_grid, size = 1000, replace = TRUE,
                          prob = post_eval)
 quantile(post_samp_grid, c(0.025, .5, 0.975))
